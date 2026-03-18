ca51eb945abb5f924a09400d8c4bfca3bf015b92
Яшенок Алина Игоревна <alina.yashenok@sd-yashenok-mac.service-it.by>
2026-03-18 17:43:34 +0300

    feat: add version at the splash screen

--- App/Sources/UI/Screens/SplashViewController.swift
+++ App/Sources/UI/Screens/SplashViewController.swift
@@ -1,21 +1,8 @@
 import UIKit
 
 final class SplashViewController: UIViewController {
-    let nameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        nameLabel.text = "TrainingApp"
-        nameLabel.textAlignment = .center
-        nameLabel.center = view.center
-        view.addSubview(nameLabel)
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { [weak self] _ in
-            self?.navigateToMainMenu()
-        }
-        
-        
-    }
+    let nameLabel = UILabel()
+    let versionLabel = UILabel()
     
     private func navigateToMainMenu() {
         guard let window = view.window else {return}
@@ -23,5 +10,37 @@ final class SplashViewController: UIViewController {
         
         let navigation = UINavigationController(rootViewController: mainMenuVC)
         window.rootViewController = navigation
+    }
+    
+    private func loadVersion() -> String? {
+        guard let path = Bundle.main.path(forResource: "version", ofType: "txt"),
+              let content = try? String(contentsOfFile: path, encoding: .utf8) else {return nil}
+        return content
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        nameLabel.text = "TrainingApp"
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        versionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let version = loadVersion() {
+            versionLabel.text = version
+        }
+        
+        view.addSubview(nameLabel)
+        view.addSubview(versionLabel)
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { [weak self] _ in
+            self?.navigateToMainMenu()
+        }
+        NSLayoutConstraint.activate([
+            nameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            nameLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -5),
+            
+            versionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+            versionLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+        
     }
 }
